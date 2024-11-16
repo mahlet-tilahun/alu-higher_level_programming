1d45e388b4a32c6a5c974d0b588f28b7664e43da
mahlet-tilahun <m.tilahun@alustudent.com>
2024-11-16 08:26:27 -0800

    Fix errors

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
--- Creates the table hbtn_0d_usa with table states.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-    PRIMARY KEY(`id`),
-    `id`   INT          NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL
+-- create database, switch to database, create table with certain constraints.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE (id),
+    PRIMARY KEY (id)
 );
