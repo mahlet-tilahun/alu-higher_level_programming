684f50a1f05fd4693da265c6f00e5b53a23ed104
mahlet-tilahun <m.tilahun@alustudent.com>
2024-12-01 08:51:55 -0800

    Advanced tasks

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""
-multiplies two matrices
+"""Defines a function that multiplies 2 matrices by using the module NumPy.
+
+Attributes:
+    m_a (matrix)
+    m_b (matrix)
 """
 
-import numpy
-"""
-This module contains useful linear algebra operations
-"""
+
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """multiplies two matrices"""
-    return numpy.matmul(m_a, m_b)
+    """Multiplies two matrices using numpy
+
+    Args:
+        m_a (matrix): first matrix
+        m_b (matrix): second matrix
+
+    Returns:
+        matrix: the product of the two matrices.
+    """
+    # m_a = ([1, 2], [3, 4])
+    # m_b = [[1, 2], [3, 4]]
+    return np.matmul(m_a, m_b)
