1b598d68692dbec9ee2f081e68658480c3f80c7a
mahlet-tilahun <m.tilahun@alustudent.com>
2024-12-01 07:58:44 -0800

    Edit script for task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"Module that fetches https://alu-intranet.hbtn.io/status"
+"""
+This script fetches the status from a given URL using the urllib library.
+"""
+
+
 import urllib.request
 
-
-url = "https://intranet.hbtn.io/status"
+url = 'https://intranet.hbtn.io/status'
 if url.startswith('https://'):
-    url = 'https://alu-intranet.hbtn.io/status
+    url = 'https://alu-intranet.hbtn.io/status'
 
 if __name__ == '__main__':
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+
+# import urllib.request
+
+# if __name__ == "__main__":
+#     url = "https://alu-intranet.hbtn.io/status"
+#     with urllib.request.urlopen(url) as request:
+#         print("Body response:")
+#         data = request.read()
+#         print("\t- type: {}".format(type(data)))
+#         print("\t- content: {}".format(data))
+#         print("\t- utf8 content: {}".format(data.decode("utf-8")))
