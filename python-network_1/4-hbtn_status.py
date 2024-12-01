7da974f714c4bec2ac12e3b4bce32d8785dae335
mahlet-tilahun <m.tilahun@alustudent.com>
2024-12-01 07:52:11 -0800

    Edit script for task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-"""fetches https://alu-intranet.hbtn.io/status using requests"""
+"""
+This script makes a request and displays a
+response using the requests package.
+"""
+
 import requests
 
-if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
-    print("Body response:$")
-    print("\t- type: <class 'str'>$")
-    print("\t- content: {}$".format(response.text.strip()))
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
 
+
+# if __name__ == "__main__":
+#     response = requests.get("https://alu-intranet.hbtn.io/status")
+
+#     print("Body response:")
+#     print(f"\t- type: {type(response.text)}")
+#     print(f"\t- content: {response.text}")
