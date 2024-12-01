9120a94aeaa7ca25292283bdd9903955772838c3
mahlet-tilahun <m.tilahun@alustudent.com>
2024-12-01 07:17:41 -0800

    Error fix

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-script that fetches https://alu-intranet.hbtn.io/status.
-"""
+"""fetches https://intranet.hbtn.io/status using requests"""
 import requests
 
 
 if __name__ == "__main__":
-    
-    r = requests.get('https://alu-intranet.hbtn.io/status')
-    
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    """fetches https://intranet.hbtn.io/status using requests"""
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
