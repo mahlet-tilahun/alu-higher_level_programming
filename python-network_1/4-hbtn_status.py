aaf75a1c972be001af44e3e25342b80fa38d1d9b
mahlet-tilahun <m.tilahun@alustudent.com>
2024-12-01 07:41:33 -0800

    Error fix

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://alu-intranet.hbtn.io/status using requests"""
+"""__summary__
+- Write a Python script that
+- fetches https://intranet.hbtn.io/status.
+"""
 import requests
 
 
 if __name__ == "__main__":
-     r = requests.get('https://intranet.hbtn.io/status')
-     t = r.text
-     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
